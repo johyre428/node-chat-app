4fd08c755e55c9fca0d6ece48cae97209bcb8fd3
Al-Johyre <al-johyre.abdullah@tooltwist.com.com>
2020-06-16 15:14:50 +0800

    Add form for messages and show incoming messages

--- server/server.js
+++ server/server.js
@@ -7,21 +7,20 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const generateMessage = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  socket.emit('newMessage', 'Welcome to the Chat App!');
-  socket.broadcast.emit('newMessage', 'New user joined');
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App!'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
   
-  socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
-      ...message,
-      createdAt: new Date().getTime()
-    });
-    
+  socket.on('createMessage', (message, callback) => {
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   ...message,
     //   createdAt: new Date().getTime()
