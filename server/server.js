474b5ce665b8737fb90bb02e14900337977bdc64
Al-Johyre <al-johyre.abdullah@tooltwist.com.com>
2020-06-16 11:11:59 +0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => console.log('Listening to port: ' + PORT));
+
