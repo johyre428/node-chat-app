dfdfb7fa48e2916d3e8a12fd2e845a7ef538a300
Al-Johyre <al-johyre.abdullah@tooltwist.com.com>
2020-06-16 20:04:51 +0800

    Add Users Class and update user list event

--- server/server.js
+++ server/server.js
@@ -8,15 +8,33 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
+const isRealString = require('./utils/validation');
+const Users = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
+let users = new Users();
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App!'));
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and Room Name are required!');
+    }
+    
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+    
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App!'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    
+    callback();
+  });
   
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
@@ -28,7 +46,12 @@ io.on('connection', (socket) => {
   })
   
   socket.on('disconnect', () => {
-    console.log('User was disconnected!');
+    const user = users.removeUser(socket.id);
+    
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`))
+    }
   });
 });
 
